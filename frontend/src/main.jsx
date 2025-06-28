c0709be7122d8709d3bca74517c50f84cd0c73b7
Danish Siddiqui <danishsidd011@gmail.com>
2025-06-28 19:35:22 +0530

    deleted index.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
-import "./index.css";
-import App from "./App.jsx";
 import { BuyersProvider } from "./context/BuyersContext";
 import { LayoutProvider } from "./context/LayoutContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -14,6 +16,17 @@ createRoot(document.getElementById("root")).render(
         <BuyersProvider>
           <BrowserRouter>
             <App />
+            <ToastContainer
+              position="top-right" // Ensure the position is set
+              autoClose={5000} // Set autoClose to 5 seconds
+              hideProgressBar={false} // Show the progress bar
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
           </BrowserRouter>
         </BuyersProvider>
       </LayoutProvider>
