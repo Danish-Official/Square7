e4d47d449cb6f184bafec90c3734edaa9a121887
Danish Siddiqui <danishsidd011@gmail.com>
2025-06-29 22:11:02 +0530

    main updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
+import { AuthProvider } from "./context/AuthContext"; 
 import { BuyersProvider } from "./context/BuyersContext";
 import { LayoutProvider } from "./context/LayoutContext";
 import { ToastContainer } from "react-toastify";
